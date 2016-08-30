57657e38f52fc6963b852c5bd5ea02d185fd54e6
Xiaoshan Hill <hilllocanton@gmail.com>
2016-08-30 18:32:25 -0400

    Debug: PlayerCollider adjust after teleport

--- Assets/Scripts/PlayerColliderFollower.cs
+++ Assets/Scripts/PlayerColliderFollower.cs
@@ -4,6 +4,16 @@ using System.Collections;
 
 public class PlayerColliderFollower : MonoBehaviour {
 
+    #region Fields
+
+    /// <summary>
+    /// The transform of CameraRig.
+    /// </summary>
+    [SerializeField]
+    private Transform CameraRigTrans;
+
+    #endregion Fields
+
     #region MonoBehaviour
 
     /// <summary>
@@ -27,7 +37,8 @@ public class PlayerColliderFollower : MonoBehaviour {
     /// </summary>
     private void PlayerFollower()
     {
-        Vector3 headPos = InputTracking.GetLocalPosition(VRNode.Head);
+        Vector3 headPos = InputTracking.GetLocalPosition(VRNode.CenterEye);
+        headPos += CameraRigTrans.position;
         headPos.y = 0.0f;
         //Debug.Log(headPos.ToString());
         this.transform.position = headPos;
